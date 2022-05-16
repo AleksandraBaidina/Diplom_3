a12a86a27c71bb82ea25043856108f513fb86b43
Aleksandra Baidina <aleksandrabaidinas@gmail.com>
2022-05-15 22:17:47 -0400

    RestorePasswordPage class was added

---
+++ src/main/java/PageObject/RestorePasswordPage.java
@@ -0,0 +1,24 @@
+package PageObject;
+
+import com.codeborne.selenide.SelenideElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.How;
+
+public class RestorePasswordPage {
+    //Текст Восстановление пароля
+    @FindBy(how = How.XPATH, using = ".//*[text()='Восстановление пароля']")
+    public SelenideElement restorePasswordText;
+
+    //Поле Email
+    @FindBy(how = How.CSS, using = ".input.pr-6.pl-6.input_type_text.input_size_default")
+    public SelenideElement emailField;
+
+    //Кнопка восстановить
+    @FindBy(how = How.CSS, using = ".button_button__33qZ0.button_button_type_primary__1O7Bx.button_button_size_medium__3zxIa")
+    public SelenideElement restoreBtn;
+
+    //Ссылка Войти
+    @FindBy(how = How.XPATH, using = ".//*[text()='Войти']")
+    public SelenideElement loginLink;
+
+}
